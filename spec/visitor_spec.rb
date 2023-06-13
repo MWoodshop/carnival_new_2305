2264da162cb4c88833010f88ea4fa166e2ca41e8
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-13 11:11:28 -0400

    test: creates initial Visitor class initialize test

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe Visitor do
+  describe '#initialize' do
+    it 'exists' do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+
+      expect(visitor1).to be_a(Visitor)
+    end
+  end
+end
