250859d1312b4d89b99e904a61da7293ca56af1a
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-13 13:01:07 -0400

    feat: adds most_profitable_ride and total_revenue methods

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -11,10 +11,25 @@ class Carnival
   end
 
   def most_popular_ride
-    # Checks if all riders logs are empty and returns {}
+    # Checks if all rider_logs are empty and returns {}
     return {} if @rides.all? { |ride| ride.rider_log.empty? }
 
     # Otherwise returns the ride with the most riders
     @rides.max_by { |ride| ride.rider_log.values.sum }
+  end
+
+  def most_profitable_ride
+    # Checks if all ride total_revenue are 0 and returns {}
+    return {} if @rides.all? { |ride| ride.total_revenue.zero? }
+
+    # Otherwise return the ride with the most total_revenue
+    @rides.max_by { |ride| ride.total_revenue }
+  end
+
+  def total_revenue
+    # Checks if all ride total_revenue are 0 and returns {}
+    return {} if @rides.all? { |ride| ride.total_revenue.zero? }
+
+    @rides.sum { |ride| ride.total_revenue }
   end
 end
