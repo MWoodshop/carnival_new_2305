a9d4a9f6e0ddfbfd3b9511bb3f9181e0810b5957
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-13 11:31:48 -0400

    test: adds empty rider_log test to class initialize

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe Ride do
       expect(ride1.admission_fee).to eq(1)
       expect(ride1.excitement).to eq(:gentle)
       expect(ride1.total_revenue).to eq(0)
+      expect(ride1.rider_log).to eq({})
     end
   end
 end
