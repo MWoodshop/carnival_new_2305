ae7c4bbbe94fb8c59e4e05ac848063d92894fc75
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-13 11:31:37 -0400

    feat: adds empty rider log hash to class initialize

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,5 +1,10 @@
 class Ride
-  attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue,
+              :rider_log
 
   def initialize(ride_info)
     @name = ride_info[:name]
@@ -7,5 +12,6 @@ class Ride
     @admission_fee = ride_info[:admission_fee]
     @excitement = ride_info[:excitement]
     @total_revenue = 0
+    @rider_log = {}
   end
 end
