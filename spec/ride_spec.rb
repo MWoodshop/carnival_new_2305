72cb6b652e98ffce6feeadb8acb092a8644322ce
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-13 12:08:07 -0400

    test: adds additional board_rider tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -42,6 +42,28 @@ RSpec.describe Ride do
     it 'will not board a rider if they do not have a matching preference' do
       ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
       visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor1.add_preference(:rough)
+
+      ride1.board_rider(visitor1)
+
+      expect(ride1.rider_log).to eq({})
+    end
+
+    it 'can add multiple riders' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      expect(ride1.rider_log).to eq({ visitor1 => 1, visitor2 => 1 })
+    end
+
+    it 'can ride multiple times' do
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
       visitor2 = Visitor.new('Tucker', 36, '$5')
       visitor1.add_preference(:gentle)
       visitor2.add_preference(:gentle)
@@ -49,6 +71,7 @@ RSpec.describe Ride do
       ride1.board_rider(visitor1)
       ride1.board_rider(visitor2)
       ride1.board_rider(visitor1)
+      expect(ride1.rider_log).to eq({ visitor1 => 2, visitor2 => 1 })
     end
   end
 end
