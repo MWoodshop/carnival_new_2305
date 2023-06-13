8a39c8c2177955c10c879538c192f60212f9191e
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-13 12:33:26 -0400

    test: adds add_ride method test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Carnival do
       expect(carnival1).to be_a(Carnival)
     end
   end
+
+  describe '#add_ride' do
+    it 'can add rides' do
+      carnival1 = Carnival.new(14)
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+      carnival1.add_ride(ride1)
+
+      expect(carnival1.rides).to eq([ride1])
+    end
+  end
 end
